908b9fa259f11d0d8925dd8a9618f2b8812dd692
darlingtonj <darlingtonj@live.co.uk>
2017-08-08 10:21:42 +0100

    Inital Commit for Personal Trainer App

---
+++ MyApplication/app/src/main/java/com/example/darli/tgm_trainer_side/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.darli.tgm_trainer_side;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
